eecb2cdfe32ec128027ed0c4082b3522aa31f202
System Administrator <root@MacBook-Pro.local>
2020-10-30 18:39:36 -0300

    Teste de imagens_4

--- resources/views/vendor/adminlte/partials/navbar/menu-item-dropdown-user-menu.blade.php
+++ resources/views/vendor/adminlte/partials/navbar/menu-item-dropdown-user-menu.blade.php
@@ -25,7 +25,7 @@
 
         @if (auth()->user()->image != null)
        
-         <img src="{{ url('storage/users/'.auth()->user()->image)}}" class="img-circle elevation-2" alt="{{ auth()->user()->name }}" height="30" width="30" > 
+         <img src="{{ url('storage/users/imagem_user_1.jpeg')}}" class="img-circle elevation-2" alt="{{ auth()->user()->name }}" height="30" width="30" > 
          
         @endif
     
