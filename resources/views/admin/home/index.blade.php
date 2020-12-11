cd70bda7d17b19327673634676fef7b3093da7b8
System Administrator <root@MacBook-Pro.local>
2020-12-11 12:02:37 -0300

    Layout home

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -10,6 +10,7 @@
 
 @section('content_header')
     <h3 class="text-dark text-center p-1">Controle Financeiro </h3>
+    <h6 class="card-subtitle text-center">Conta:  {{ auth()->user()->name }}</h6>
 @stop
 
 @section('content')
@@ -20,11 +21,10 @@
         <div class="col-md-4 col-sm-12">
         <a href="{{ route('despesa.index') }}" class="">
             <div class="card">
-                <h5 class="mt-2 text-center">Caixa: Registrar Despesas</h5>
+                <h5 class="mt-2 text-center">Registrar Despesas</h5>
                  <img class="card-img-top img-responsive img-thumbnail" src="{{ asset('img/cards/despesas.jpeg')}}"  style="height: 200px;"alt="Espaço reservado para exibição de imagens" >
                 <div class="card-body">
            
-                    <h6 class="card-subtitle text-center">Conta:  {{ auth()->user()->name }}</h6>
                 </div>
             </div>
         </a>
@@ -34,11 +34,10 @@
         <div class="col-md-4 col-sm-12">
             <a href="{{ route('receita.index') }}" class="">
                 <div class="card">
-                    <h5 class="mt-2 text-center">Caixa: Registrar Receitas</h5>           
+                    <h5 class="mt-2 text-center">Registrar Receitas</h5>           
                         <img class="card-img-top img-responsive img-thumbnail" src="{{ asset('img/cards/receitas.jpeg')}}"  style="height: 200px;" alt="Espaço reservado para exibição de imagem" >              
                     <div class="card-body">
 
-                        <h6 class="card-subtitle text-center">Conta:  {{ auth()->user()->name }}</h6>
                     </div>
                 </div>
              </a>
@@ -47,13 +46,12 @@
         <div class="col-md-4 col-sm-12">
             <a href="{{ route('financeiro.fluxoDeCaixa') }}" class="">
             <div class="card">
-                <h5 class="mt-2 text-center">Fluxo de Caixa</h5>
+                <h5 class="mt-2 text-center">Registrar Investimentos</h5>
     
-                    <img class="card-img-top img-responsive img-thumbnail" src="{{ asset('img/cards/fluxo_de_caixa.jpeg')}}"  style="height: 200px;" alt="Espaço reservado para exibição de imagem" >
+                    <img class="card-img-top img-responsive img-thumbnail" src="{{ asset('img/cards/fluxo_futuro.jpeg')}}"  style="height: 200px;" alt="Espaço reservado para exibição de imagem" >
                   
                 <div class="card-body">
     
-                    <h6 class="card-subtitle text-center">Conta:  {{ auth()->user()->name }}</h6>
                 </div>
             </div>
         </a>
@@ -62,11 +60,10 @@
         <div class="col-md-4 col-sm-12">
             <a href="{{ route('despesa.index') }}" class="">
                 <div class="card">
-                    <h5 class="mt-2 text-center">Caixa: Registrar Despesas</h5>
-                     <img class="card-img-top img-responsive img-thumbnail" src="{{ asset('img/cards/despesas.jpeg')}}"  style="height: 200px;"alt="Espaço reservado para exibição de imagens" >
+                    <h5 class="mt-2 text-center">Fluxo de Caixa</h5>
+                     <img class="card-img-top img-responsive img-thumbnail" src="{{ asset('img/cards/fluxo_de_caixa.jpeg')}}"  style="height: 200px;"alt="Espaço reservado para exibição de imagens" >
                     <div class="card-body">
                
-                        <h6 class="card-subtitle text-center">Conta:  {{ auth()->user()->name }}</h6>
                     </div>
                 </div>
             </a>
